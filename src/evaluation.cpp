ba2e94591ef320a081c4ad4e86e890082c30dff0
davidYichengWei <a1093888167@hotmail.com>
2024-03-30 18:57:41 -0400

    build: 👷 set up filter build

---
+++ src/evaluation.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+#include "filter/bloom/bloom.h"
+// #include "filter/vector_quotient_filter/vqf_cpp.h"
+
+int main() {
+  std::cout << "Hello, World!" << std::endl;
+  return 0;
+}
